d96511bf6a730756bec409f6d9a12d687206e4dd
ant__on <antonustimov@gmail.com>
2021-01-16 14:33:57 +0300

    all leights calculated

--- lesson_002/06_songs_list.py
+++ lesson_002/06_songs_list.py
@@ -22,7 +22,9 @@ violator_songs_list = [
 
 total_leight_1 = str(round(violator_songs_list[3][1] + violator_songs_list[5][1] + violator_songs_list[-1][1]))
 
-print("total leight of 3 songs is " + total_leight_1)
+print("total leight of 3 songs is " + total_leight_1 + " min")
+
+
 # Есть словарь песен группы Depeche Mode
 violator_songs_dict = {
     'World in My Eyes': 4.76,
@@ -38,5 +40,7 @@ violator_songs_dict = {
 
 # распечатайте общее время звучания трех песен: 'Sweetest Perfection', 'Policy of Truth' и 'Blue Dress'
 #   А другие три песни звучат ХХХ минут
+total_leight_2 = str(round(violator_songs_dict['Sweetest Perfection'] + violator_songs_dict['Policy of Truth'] +
+                           violator_songs_dict['Blue Dress']))
 
-# TODO здесь ваш код
+print('ant another 3 sogs leight is ' + total_leight_2 + " min")
