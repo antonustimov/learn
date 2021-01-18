1ac46a7806ae6d4e170b86ef5ddb8cc9bad37047
ant__on <antonustimov@gmail.com>
2021-01-18 19:36:59 +0300

    wall of bricks done

---
+++ lesson_003/07_wall.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+
+import simple_draw as sd
+sd.resolution = (1200, 600)
+
+# Нарисовать стену из кирпичей. Размер кирпича - 100х50
+# Использовать вложенные циклы for
+brick_height = 50
+for y in range(0, 600, 50):
+    if y / 50 % 2 == 0:
+        shift = 50
+    else: shift = 0
+    horisontal_start_point = sd.get_point(1, y)
+    horisontal_end_point = sd.get_point(1200, y)
+    sd.line(start_point=horisontal_start_point, end_point=horisontal_end_point, color=sd.COLOR_RED, width=2)
+    for x in range(1, 1200, 100):
+        vertical_start_point = sd.get_point(x + shift, y)
+        vertical_end_point = sd.get_point(x + shift, y + brick_height)
+        sd.line(start_point=vertical_start_point, end_point=vertical_end_point, color=sd.COLOR_RED, width=2)
+
+
+
+
+
+print("we don't need your education")
+sd.pause()
