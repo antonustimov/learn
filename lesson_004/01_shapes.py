bffd1d327c1259a881d99673256d277c8ad847b0
ant__on <antonustimov@gmail.com>
2021-01-27 16:10:59 +0300

    shapes def ready

--- lesson_004/01_shapes.py
+++ lesson_004/01_shapes.py
@@ -8,61 +8,61 @@ sd.resolution = (1200, 600)
 # - треугольника
 point_0 = sd.get_point(600, 100)
 length = 200
-
-def triangle(start_point, angle, length):
-    v_1 = sd.get_vector(start_point=start_point, angle=angle, length=length, width=3)
-    v_1.draw()
-    v_2 = sd.get_vector(start_point=v_1.end_point, angle=angle + 120, length=length, width=3)
-    v_2.draw()
-    v_3 = sd.get_vector(start_point=v_2.end_point, angle=angle + 240, length=length, width=3)
-    v_3.draw()
-
-
-triangle(start_point=point_0, angle=30, length=length)
-# - квадрата
-
-def square(start_point, angle, length):
-    v_1 = sd.get_vector(start_point=start_point, angle=angle, length=length, width=3)
-    v_1.draw()
-    v_2 = sd.get_vector(start_point=v_1.end_point, angle=angle + 90, length=length, width=3)
-    v_2.draw()
-    v_3 = sd.get_vector(start_point=v_2.end_point, angle=angle + 180, length=length, width=3)
-    v_3.draw()
-    v_4 = sd.get_vector(start_point=v_3.end_point, angle=angle + 270, length=length, width=3)
-    v_4.draw()
-
-
-square(start_point=point_0, angle=30, length=length)
-# - пятиугольника
-def pentacle(start_point, angle, length):
-    v_1 = sd.get_vector(start_point=start_point, angle=angle, length=length, width=3)
-    v_1.draw()
-    v_2 = sd.get_vector(start_point=v_1.end_point, angle=angle + 72, length=length, width=3)
-    v_2.draw()
-    v_3 = sd.get_vector(start_point=v_2.end_point, angle=angle + 144, length=length, width=3)
-    v_3.draw()
-    v_4 = sd.get_vector(start_point=v_3.end_point, angle=angle + 144 + 72, length=length, width=3)
-    v_4.draw()
-    v_5 = sd.get_vector(start_point=v_4.end_point, angle=angle + 288, length=length, width=3)
-    v_5.draw()
-
-pentacle(start_point=point_0, angle=30, length=length)
-# - шестиугольника
-def sixangle(start_point, angle, length):
-    v_1 = sd.get_vector(start_point=start_point, angle=angle, length=length, width=3)
-    v_1.draw()
-    v_2 = sd.get_vector(start_point=v_1.end_point, angle=angle + 60, length=length, width=3)
-    v_2.draw()
-    v_3 = sd.get_vector(start_point=v_2.end_point, angle=angle + 120, length=length, width=3)
-    v_3.draw()
-    v_4 = sd.get_vector(start_point=v_3.end_point, angle=angle + 180, length=length, width=3)
-    v_4.draw()
-    v_5 = sd.get_vector(start_point=v_4.end_point, angle=angle + 240, length=length, width=3)
-    v_5.draw()
-    v_6 = sd.get_vector(start_point=v_5.end_point, angle=angle + 300, length=length, width=3)
-    v_6.draw()
-
-sixangle(start_point=point_0, angle=30, length=length)
+#
+# def triangle(start_point, angle, length):
+#     v_1 = sd.get_vector(start_point=start_point, angle=angle, length=length, width=3)
+#     v_1.draw()
+#     v_2 = sd.get_vector(start_point=v_1.end_point, angle=angle + 120, length=length, width=3)
+#     v_2.draw()
+#     v_3 = sd.get_vector(start_point=v_2.end_point, angle=angle + 240, length=length, width=3)
+#     v_3.draw()
+#
+#
+# triangle(start_point=point_0, angle=30, length=length)
+# # - квадрата
+#
+# def square(start_point, angle, length):
+#     v_1 = sd.get_vector(start_point=start_point, angle=angle, length=length, width=3)
+#     v_1.draw()
+#     v_2 = sd.get_vector(start_point=v_1.end_point, angle=angle + 90, length=length, width=3)
+#     v_2.draw()
+#     v_3 = sd.get_vector(start_point=v_2.end_point, angle=angle + 180, length=length, width=3)
+#     v_3.draw()
+#     v_4 = sd.get_vector(start_point=v_3.end_point, angle=angle + 270, length=length, width=3)
+#     v_4.draw()
+#
+#
+# square(start_point=point_0, angle=30, length=length)
+# # - пятиугольника
+# def pentacle(start_point, angle, length):
+#     v_1 = sd.get_vector(start_point=start_point, angle=angle, length=length, width=3)
+#     v_1.draw()
+#     v_2 = sd.get_vector(start_point=v_1.end_point, angle=angle + 72, length=length, width=3)
+#     v_2.draw()
+#     v_3 = sd.get_vector(start_point=v_2.end_point, angle=angle + 144, length=length, width=3)
+#     v_3.draw()
+#     v_4 = sd.get_vector(start_point=v_3.end_point, angle=angle + 144 + 72, length=length, width=3)
+#     v_4.draw()
+#     v_5 = sd.get_vector(start_point=v_4.end_point, angle=angle + 288, length=length, width=3)
+#     v_5.draw()
+#
+# pentacle(start_point=point_0, angle=30, length=length)
+# # - шестиугольника
+# def sixangle(start_point, angle, length):
+#     v_1 = sd.get_vector(start_point=start_point, angle=angle, length=length, width=3)
+#     v_1.draw()
+#     v_2 = sd.get_vector(start_point=v_1.end_point, angle=angle + 60, length=length, width=3)
+#     v_2.draw()
+#     v_3 = sd.get_vector(start_point=v_2.end_point, angle=angle + 120, length=length, width=3)
+#     v_3.draw()
+#     v_4 = sd.get_vector(start_point=v_3.end_point, angle=angle + 180, length=length, width=3)
+#     v_4.draw()
+#     v_5 = sd.get_vector(start_point=v_4.end_point, angle=angle + 240, length=length, width=3)
+#     v_5.draw()
+#     v_6 = sd.get_vector(start_point=v_5.end_point, angle=angle + 300, length=length, width=3)
+#     v_6.draw()
+#
+# sixangle(start_point=point_0, angle=30, length=length)
 # Все функции должны принимать 3 параметра:
 # - точка начала рисования
 # - угол наклона
@@ -101,6 +101,30 @@ sixangle(start_point=point_0, angle=30, length=length)
 #
 # Не забудте в этой общей функции придумать, как устранить разрыв
 #   в начальной/конечной точках рисуемой фигуры (если он есть)
+number_of_angles = int(input("Type number of angles from 3 to 8 pls: "))
+while number_of_angles < 3:
+    print("You enter wrong data! Please try again")
+    number_of_angles = int(input("Type number of angles from 3 to 8 pls: "))
+
+side_length = int(input("Type length of each side pls: "))
+
+
+
+
+def shape(number_of_angles, start_point, angle_0, length):
+    side_angle = round(360 / number_of_angles)
+    next_angle = angle_0
+    print(angle_0)
+    if next_angle > 360:
+        return
+    v_1 = sd.get_vector(start_point=start_point, angle=next_angle, length=length)
+    v_1.draw()
+    next_angle += side_angle
+    shape(number_of_angles=number_of_angles, start_point=v_1.end_point, angle_0=next_angle, length=side_length)
+
+shape(number_of_angles=number_of_angles, start_point=sd.get_point(600, 100), angle_0=20, length=side_length)
+
+
 
 # Часть 2-бис.
 # А теперь - сколько надо работы что бы внести изменения в код? Выгода на лицо :)
