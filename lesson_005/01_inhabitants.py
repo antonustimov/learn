44a7c7befdc98e6995bc63378c211252f567d760
ant__on <antonustimov@gmail.com>
2021-02-04 14:53:37 +0300

    folks from rooms printed

---
+++ lesson_005/01_inhabitants.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+
+# Вывести на консоль жителей комнат (модули room_1 и room_2)
+# Формат: В комнате room_1 живут: ...
+
+from room_1 import folks as a
+from room_2 import folks as b
+
+print('в комнате room_1 живут ', a[0],', ', a[1])
+print('в комнате room_2 живет ', b[0])
+
+
+
+
