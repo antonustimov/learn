4806f0d26398655b31c58cf6bd8fa245884491d9
ant__on <antonustimov@gmail.com>
2021-02-05 20:49:03 +0300

    burgers ready

---
+++ lesson_005/my_burger.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+
+def bun():
+    print('сначала берем булочку')
+    return "bun"
+
+def patty():
+    print('добавляем сочную котлету')
+    return "patty"
+
+def tomato():
+    print('добавим сочный помидор')
+    return 'tomato'
+
+def cucumber():
+    print('добавляем свежий огурчик')
+    return 'cucumber'
+
+def cheese():
+    print('добавим сыр')
+    return 'cheese'
+
+def sause():
+    print('польем это все соусом')
+    return 'sause'
