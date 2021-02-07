74d4b269e3daf9e9bd28479d00dea90ec17254ae
ant__on <antonustimov@gmail.com>
2021-02-07 15:29:10 +0300

    nim ready

---
+++ lesson_006/python_snippets/nim_interface.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+
+# Ним — математическая игра, в которой два игрока по очереди берут предметы,
+# разложенные на несколько кучек. За один ход может быть взято любое количество предметов
+# (большее нуля) из одной кучки. Выигрывает игрок, взявший последний предмет.
+# В классическом варианте игры число кучек равняется трём.
+
+
+from nim_engine import put_stones, take_stone, stones_position, is_gameover
+from termcolor import cprint
+
+cprint('ДА НАЧНЕТСЯ ИГРА!!!', color='red')
+put_stones()
+counter = 1
+while True:
+    cprint('Текущая позиция:', color='yellow', on_color='on_grey')
+    cprint(stones_position(), color='red')
+    color = 'blue' if counter == 1 else 'green'
+    cprint('Ходит игрок номер'.format(counter), color=color)
+    cprint('Из какой кучки берем?', color=color)
+    stack_number = int(input()) - 1
+    cprint('Сколько берем?', color=color)
+    number_of_stones = int(input())
+    take_stone(stack_number=stack_number, number_of_stones=number_of_stones)
+
+    if is_gameover():
+        break
+    counter = 2 if counter == 1 else 1
+
+cprint('Победил игрок номер ' + str(counter) + '!!!', color=color, on_color='on_grey')
+
