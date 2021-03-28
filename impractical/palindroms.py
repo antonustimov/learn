abce3bf76f994ad2ad33352a1230eeb27983fdaf
ant__on <antonustimov@gmail.com>
2021-03-28 18:07:58 +0300

    it was too easy... open file take more text strings than need to find all palindroms

--- impractical/palindroms.py
+++ impractical/palindroms.py
@@ -1,3 +1,17 @@
 from open_as_list import opener
+from pprint import pprint
 
-print(opener('/Users/ant__on/PycharmProjects/learn/113k_dict.txt'))
+
+
+
+
+
+
+
+file_for_analyze = opener('/Users/ant__on/PycharmProjects/learn/113k_dict.txt')
+list_of_palindroms = []
+for word in file_for_analyze:
+    if word[:] == word[::-1]:
+        list_of_palindroms.append(word)
+
+pprint(list_of_palindroms)
